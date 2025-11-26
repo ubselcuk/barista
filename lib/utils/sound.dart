9608d3a437e4d614e9b74e7ef72b68704b11545d
ubselcuk <ubselcuk@gmail.com>
2025-11-26 11:53:49 +0300

    Refactor sound class methods.

--- lib/utils/sound.dart
+++ lib/utils/sound.dart
@@ -9,43 +9,56 @@ import 'package:http/http.dart' as http;
 class Sound {
   late final AudioPlayer _player;
 
-  Sound.cacheLoop(String? url) {
+  Sound.loop() {
     _player = AudioPlayer();
     _player.setReleaseMode(ReleaseMode.loop);
-    cacheAndPlay(url);
   }
 
-  Sound.cacheSingle(String? url) {
+  Sound.single() {
     _player = AudioPlayer();
     _player.setReleaseMode(ReleaseMode.release);
-    cacheAndPlay(url);
   }
 
-  void dispose() {
-    _player.dispose();
+  Sound.shout(String? url) {
+    _player = AudioPlayer();
+    _player.setReleaseMode(ReleaseMode.release);
+    cache(url).then((S) => playSource(S));
+  }
+
+  Future<void> dispose() async {
+    await _player.dispose();
     log('Sound disposed');
   }
 
-  void stop() {
-    _player.stop();
+  Future<void> playSource(DeviceFileSource? source) async {
+    if (source == null) {
+      log('No sound source available to play');
+      return;
+    }
+    await _player.play(source);
+    log('Sound playing');
+  }
+
+  Future<void> stop() async {
+    await _player.stop();
     log('Sound stopped');
   }
 
-  void pause() {
-    _player.pause();
+  Future<void> pause() async {
+    await _player.pause();
     log('Sound paused');
   }
 
-  void resume() {
-    _player.resume();
+  Future<void> resume() async {
+    await _player.resume();
     log('Sound resumed');
   }
 
-  Future<void> cacheAndPlay(String? url) async {
+  Future<DeviceFileSource?> cache(String? url) async {
     try {
       if (url == null || url.isEmpty) {
         log('Sound URL is null or empty');
-        return;
+        return null;
       }
 
       final dir = await getTemporaryDirectory();
@@ -55,14 +68,14 @@ class Sound {
       final f = File(file);
       if (await f.exists()) {
         log('Sound playing from cache');
-        _player.play(DeviceFileSource(f.path));
+        return DeviceFileSource(f.path);
       } else {
         log('Downloading sound from URL: $url');
         final response = await http.get(Uri.parse(url));
         if (response.statusCode == 200) {
           final f = await File(file).writeAsBytes(response.bodyBytes);
           log('Sound downloaded and playing');
-          _player.play(DeviceFileSource(f.path));
+          return DeviceFileSource(f.path);
         } else {
           log('Failed to download sound: ${response.statusCode}');
         }
@@ -70,5 +83,6 @@ class Sound {
     } catch (e) {
       log('Error in cacheAndPlay: $e');
     }
+    return null;
   }
 }
